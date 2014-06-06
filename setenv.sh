ac09bc0a505a9ce8d5719dc948bd8778fbea2668
Colin Stebbins Gordon <csgordon@cs.washington.edu>
2014-06-06 14:55:52 -0700

    check in work so far

---
+++ setenv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PATH=/opt/z3/bin:$PATH
+source .hsenv/bin/activate
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+hsenv
+source .hsenv/bin/activate
+echo Assuming z3 is already in path
+git clone ssh://git@github.com/ucsd-progsys/liquid-fixpoint.git
+git clone ssh://git@github.com/ucsd-progsys/liquidhaskell.git
+cd liquid-fixpoint
+cabal install
+cd ..
+cd liquidhaskell
+cabal install
+cd ..
