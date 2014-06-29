218b12a6fcd2426b728a619edbbeade95b975434
Colin Stebbins Gordon <csgordon@cs.washington.edu>
2014-06-29 15:55:03 -0700

    file reorg; test script

--- test.sh
+++ test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-PASS = true
-
-for f in RG.hs RGTests.hs MonotonicCounter.hs;
-do liquid $f;
+for f in RG.hs RGTests.hs examples/MonotonicCounter.hs examples/LockfreeMonotonicCounter.hs examples/STMLog.hs examples/CASList.hs;
+do 
+liquid $f &> /dev/null;
+if [ "$?" -eq "0" ];
+then
+    echo $f: PASS;
+elif [ "$?" -eq "1" ];
+then
+    echo $f: WARNING, nontermination or false
+else
+    echo $f: FAIL;
+fi
 done
